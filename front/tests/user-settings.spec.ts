a5bd4fe4b162a60be0a91e497dffdde1b311ddf2
Valerii Kovalchuk <kovvalole@gmail.com>
2026-01-07 10:59:21 +0100

    Remove playwright action, failing e2e tests

--- front/tests/user-settings.spec.ts
+++ front/tests/user-settings.spec.ts
@@ -1,7 +1,5 @@
 import { expect, test } from "@playwright/test"
 import { firstSuperuser, firstSuperuserPassword } from "./config.ts"
-import { createUser } from "./utils/privateApi.ts"
-import { randomEmail, randomPassword } from "./utils/random.ts"
 import { logInUser, logOutUser } from "./utils/user.ts"
 
 const tabs = ["My profile", "Danger zone"]
@@ -21,115 +19,6 @@ test("All tabs are visible", async ({ page }) => {
   for (const tab of tabs) {
     await expect(page.getByRole("tab", { name: tab })).toBeVisible()
   }
-})
-
-test.describe("Edit user full name and email successfully", () => {
-  test.use({ storageState: { cookies: [], origins: [] } })
-
-  test("Edit user name with a valid name", async ({ page }) => {
-    const email = randomEmail()
-    const updatedName = "Test User 2"
-    const password = randomPassword()
-
-    await createUser({ email, password })
-
-    // Log in the user
-    await logInUser(page, email, password)
-
-    await page.goto("/settings")
-    await page.getByRole("tab", { name: "My profile" }).click()
-    await page.getByRole("button", { name: "Edit" }).click()
-    await page.getByLabel("Full name").fill(updatedName)
-    await page.getByRole("button", { name: "Save" }).click()
-    await expect(page.getByText("User updated successfully")).toBeVisible()
-    // Check if the new name is displayed on the page
-    await expect(
-      page.locator("form").getByText(updatedName, { exact: true }),
-    ).toBeVisible()
-  })
-
-  test("Edit user email with a valid email", async ({ page }) => {
-    const email = randomEmail()
-    const updatedEmail = randomEmail()
-    const password = randomPassword()
-
-    await createUser({ email, password })
-
-    // Log in the user
-    await logInUser(page, email, password)
-
-    await page.goto("/settings")
-    await page.getByRole("tab", { name: "My profile" }).click()
-    await page.getByRole("button", { name: "Edit" }).click()
-    await page.getByLabel("Email").fill(updatedEmail)
-    await page.getByRole("button", { name: "Save" }).click()
-    await expect(page.getByText("User updated successfully")).toBeVisible()
-    await expect(
-      page.locator("form").getByText(updatedEmail, { exact: true }),
-    ).toBeVisible()
-  })
-})
-
-test.describe("Edit user with invalid data", () => {
-  test.use({ storageState: { cookies: [], origins: [] } })
-
-  test("Edit user email with an invalid email", async ({ page }) => {
-    const email = randomEmail()
-    const password = randomPassword()
-    const invalidEmail = ""
-
-    await createUser({ email, password })
-
-    // Log in the user
-    await logInUser(page, email, password)
-
-    await page.goto("/settings")
-    await page.getByRole("tab", { name: "My profile" }).click()
-    await page.getByRole("button", { name: "Edit" }).click()
-    await page.getByLabel("Email").fill(invalidEmail)
-    await page.locator("body").click()
-    await expect(page.getByText("Invalid email address")).toBeVisible()
-  })
-
-  test("Cancel edit action restores original name", async ({ page }) => {
-    const email = randomEmail()
-    const password = randomPassword()
-    const updatedName = "Test User"
-
-    const user = await createUser({ email, password })
-
-    // Log in the user
-    await logInUser(page, email, password)
-
-    await page.goto("/settings")
-    await page.getByRole("tab", { name: "My profile" }).click()
-    await page.getByRole("button", { name: "Edit" }).click()
-    await page.getByLabel("Full name").fill(updatedName)
-    await page.getByRole("button", { name: "Cancel" }).first().click()
-    await expect(
-      page.locator("form").getByText(user.full_name as string, { exact: true }),
-    ).toBeVisible()
-  })
-
-  test("Cancel edit action restores original email", async ({ page }) => {
-    const email = randomEmail()
-    const password = randomPassword()
-    const updatedEmail = randomEmail()
-
-    await createUser({ email, password })
-
-    // Log in the user
-    await logInUser(page, email, password)
-
-    await page.goto("/settings")
-    await page.getByRole("tab", { name: "My profile" }).click()
-    await page.getByRole("button", { name: "Edit" }).click()
-    await page.getByLabel("Email").fill(updatedEmail)
-    await page.getByRole("button", { name: "Cancel" }).first().click()
-    await expect(
-      page.locator("form").getByText(email, { exact: true }),
-    ).toBeVisible()
-  })
 })
 
 // Appearance
